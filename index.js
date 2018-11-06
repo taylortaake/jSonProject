6f9c72d83ccc88b8cf886696d37288804daf0488
Taylor Taake <tt0874535@otc.edu>
2018-11-05 22:53:54 -0600

    readded lodash

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ readJSON('data.json', function(error, manifest){
 })
 
 
-var fs = require("fs");
-
-// calling lodash
+// Load the full build.
 var _ = require('lodash');
+// Load the core build.
+var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+ 
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
 
-var items = fs.readFileSync("data.json");
+var readItems = fs.readFileSync("data.json");
 
-var jsonData = JSON.parse(contents);
+var jsonData = JSON.parse(readItems);
 items = jsonData.items;
 var i = 0;
 _.forEach(items, function (items, i) {
